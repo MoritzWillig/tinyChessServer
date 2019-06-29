7510bc48ed6eacf1ece47be88ee28bfb5cb4d134
Tim Schneider <tim.schneider94@t-online.de>
2019-06-29 21:06:52 +0200

    Made CLI stuff work with sjeng engine

--- gameserver.js
+++ gameserver.js
@@ -623,8 +623,8 @@ class GameServer {
           }
         });
         break;
-      default:
-        client.sendMessage("Error (unkown command): Ignored");
+      //default:
+      //  client.sendMessage("Error (unkown command): Ignored");
     }
   }
   
